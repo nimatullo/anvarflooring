b1050d7c5ce9d3495c660caf0af515038dec9f61
Azizbek-Ibragimov <126601882+Azizbek-Ibragimov@users.noreply.github.com>
2025-11-09 19:58:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ document.addEventListener('DOMContentLoaded', () => {
         'magic flooring 100.jpg',
         'magic flooring 101.jpg',
         'magic flooring 102.jpg',
-        'magic flooring 103.jpg'
+        'magic flooring 103.jpg',
+        'island.mov'
         
         
     ]
